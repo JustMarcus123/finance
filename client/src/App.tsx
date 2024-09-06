06c229c448a5f94367c16a5ca8bf47750849a5fd
JustMarcus123 <141229878+JustMarcus123@users.noreply.github.com>
2024-09-06 14:34:08 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import './App.css'
+import { Dashboard } from "./pages/dashboard"
+import { Auth } from "./pages/auth"
+
+function App() {
+
+
+  return (
+   <Router>
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/auth" element={<Auth/>}/>
+
+      </Routes>
+    </div>
+   </Router>
+  )
+}
+
+export default App
+
+
---
+++ client/src/pages/dashboard/financial-record-list.tsx
@@ -0,0 +1,8 @@
+
+export const FinancialRecordList =()=>{
+    return(
+        <div>
+            Record List
+        </div>
+    )
+}
